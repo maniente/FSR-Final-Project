00decabdde588ea6ef97bc766521568c4f90759c
carlobarone00 <carlobaronee@gmail.com>
2025-06-20 11:52:23 +0200

    Add files via upload

--- animate.m
+++ animate.m
@@ -3,12 +3,23 @@ function [] = animate(T, param,X)
 nSteps = length(T);
 Xc_all = zeros(param.N, nSteps);  % Dove N è il numero di link
 Yc_all = zeros(param.N, nSteps);
+FRX = zeros(param.N,nSteps);       % Forza propulsiva - senza pareti - su x
+FRY = zeros(param.N,nSteps);       % Forza propulsiva - senza pareti - su y
+FCN = zeros(param.N, nSteps);           % forza normale (contatto parete)
+FCT = zeros(param.N, nSteps);           % forza tangenziale (contatto parete)
+
 for k = 1:nSteps
     x_k = X(k,:)';  % vettore colonna
-    [~, x_c, y_c, ~, ~, ~] = dynamicModel(T(k), x_k, param);    
+    [~, x_c, y_c, fr, fcontact, ~] = dynamicModel(T(k), x_k, param);    
     Xc_all(:,k) = x_c;
     Yc_all(:,k) = y_c;
+    FR(:,k) = fr(1,:)';                        % Forze propulsiva - senza pareti
+    FRX(:,k) = fr(1:param.N);                  % Forza propulsiva - senza pareti - su x
+    FRY(:,k) = fr(param.N+1:end);              % Forza propulsiva - senza pareti - su y
+    FCT(:,k) = fcontact(1:param.N);            % Forze tangenziali da parete
+    FCN(:,k) = fcontact(param.N+1:end);        % Forze normali da parete
 end
+
 if param.Plot3D == 0
 %% Graphic Visualization of the Snake
 x_history = [];
@@ -41,8 +52,25 @@ for k = 1:length(T)
     clf;
     hold on; grid on;axis square;axis equal;
 
+    scale_force = 0.5;  % fattore di scala per la lunghezza delle frecce
     % Se attivo il contatto con le pareti, disegna i bordi del tubo
-    if param.contact == 1
+    if param.contact == 0
+        for i = 1:param.N
+            fr_x = FRX(i,k);
+            fr_y = FRY(i,k);
+        if abs(fr_x) > 1e-4 || (fr_y) > 1e-4
+            xc = Xc_all(i, k);
+            yc = Yc_all(i, k);
+
+            %Forza tangenziale: verso x
+            quiver(xc, yc, 50*fr_x, 0, 0, 'Color', 'b', 'LineWidth', 2, 'MaxHeadSize', 1);
+            
+            %Forza normale: verso y
+            quiver(xc, yc, 0, 50*fr_y, 0, 'Color', 'r', 'LineWidth', 2, 'MaxHeadSize', 1);
+        end
+        end
+    else
+
         % Lunghezza del tubo (visualizzazione): centrata sul baricentro
         x_c = mean(Xc_all(:,k));
         L_view = 2;  % lunghezza visiva del tratto del tubo
@@ -54,8 +82,26 @@ for k = 1:length(T)
         y2 = +param.diameter/2;
 
         % Disegna le due pareti come linee
-        plot([x_min, x_max], [y1, y1], 'k--', 'LineWidth', 2);  % parete inferiore
-        plot([x_min, x_max], [y2, y2], 'k--', 'LineWidth', 2);  % parete superiore
+        plot([x_min, x_max], [y1, y1], 'k--', 'LineWidth', 1);  % parete inferiore
+        plot([x_min, x_max], [y2, y2], 'k--', 'LineWidth', 1);  % parete superiore
+
+        
+        for i = 1:param.N
+        fn = FCN(i,k);
+        ft = FCT(i,k);
+
+        % Solo se c'è una forza non nulla
+        if abs(fn) > 1e-4 || abs(ft) > 1e-4
+            xc = Xc_all(i, k);
+            yc = Yc_all(i, k);
+
+            % Forza tangenziale: verso x
+            quiver(xc, yc, scale_force*ft, 0, 0, 'Color', 'b', 'LineWidth', 2, 'MaxHeadSize', 1);
+            % 
+            % % Forza normale: verso y
+            quiver(xc, yc, 0, scale_force*fn, 0, 'Color', 'r', 'LineWidth', 2, 'MaxHeadSize', 1);
+        end
+        end
     end
     title(sprintf('t = %.2f s', T(k)));
     
@@ -103,15 +149,9 @@ close(vidfile);
 
 %% Impostazioni per animazione 3D
 else
-param.dimensionPlot3D = 1;  % Abilita la modalità 3D
-nSteps = length(T);
-Xc_all = zeros(param.N, nSteps);  % Dove N è il numero di link
-Yc_all = zeros(param.N, nSteps);
 Zc_all = zeros(param.N, nSteps);  % Aggiunta coordinata Z
 
-for k = 1:nSteps
-    x_k = X(k,:)';  % vettore colonna
-    [~, x_c,y_c] = dynamicModel(T(k), x_k, param);
+for k = 1:nSteps    
     %% Per animazione 3D
     % Calcola la posizione Z (altezza nel tubo)
     z_c = zeros(param.N,1);
@@ -119,8 +159,6 @@ for k = 1:nSteps
         % Modello semplificato per altezza nel tubo (puoi personalizzare)
         z_c(i) = 0.1 * sin(0.5 * T(k) + i * 0.2);
     end
-    Xc_all(:,k) = x_c;
-    Yc_all(:,k) = y_c;
     Zc_all(:,k) = z_c;
 end
 
@@ -129,30 +167,34 @@ vidfile = VideoWriter(name, 'Motion JPEG AVI');
 open(vidfile);
 
 % Crea figura 3D con dimensioni fisse
-fig = figure('Position', [100, 100, 560, 420], ...
-             'MenuBar', 'none', ...
-             'ToolBar', 'none', ...
-             'Resize', 'off');
-         
-axis tight manual;
+figure('Position',[200 100 1000 600]);
+set(0, 'DefaultFigureRenderer', 'opengl');
+set(gcf, 'Color', 'white')
+
+axis tight manual;  % Impedisce cambiamenti automatici degli assi
 hold on;
 grid on;
 axis equal;
+
 view(3);  % Vista 3D
 xlabel('X');
 ylabel('Y');
 zlabel('Z');
 title('Snake Robot in Pipeline - 3D Simulation');
 
-% Disegna il tubo (pipeline)
-tube_radius = param.diameter/2;
-tube_length = max(Xc_all(:)) - min(Xc_all(:)) + 2;
-[x_tube, y_tube, z_tube] = cylinder(tube_radius, 50);
-z_tube = z_tube * tube_length;
-surf(z_tube, y_tube, x_tube, ...
-    'FaceAlpha', 0.2, ...
-    'EdgeColor', 'none', ...
-    'FaceColor', [0.7 0.7 1]);
+if param.contact == 1
+    % Green pipeline like your figure
+    tube_radius = param.diameter / 2;
+    tube_length = max(Xc_all(:)) - min(Xc_all(:)) + 2;
+    [x_tube, y_tube, z_tube] = cylinder(tube_radius, 50);
+    z_tube = z_tube * tube_length;
+
+    % Rotate cylinder: make it horizontal along X-axis
+    surf(z_tube + min(Xc_all(:)) - 1, y_tube, x_tube, ...
+        'FaceAlpha', 0.3, ...
+        'EdgeColor', 'none', ...
+        'FaceColor', [0.2 1 0.2]);  % bright green
+end
 
 % Imposta limiti assi
 xlim([min(Xc_all(:))-0.5, max(Xc_all(:))+0.5]);
@@ -162,9 +204,8 @@ zlim([-tube_radius-0.1, tube_radius+0.1]);
 for k = 1:length(T)
     clf(fig);
     set(fig, 'Color', 'w');
-    hold on;
-    grid on;
-    axis equal;
+    hold on; grid on;axis square;axis equal;
+
     view(3);
     xlabel('X');
     ylabel('Y');
@@ -206,8 +247,7 @@ for k = 1:length(T)
     zlim([-tube_radius-0.1, tube_radius+0.1]);
     
     % Cattura frame e scrivi nel video
-    frame = getframe(fig);
-    writeVideo(vidfile, frame);
+    writeVideo(vidfile, getframe(gcf));
 
     drawnow;
 end
